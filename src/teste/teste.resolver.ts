43b04fdc704998b57017df36424a6fc3dde26f2a
Raul Burigo <raul.burigo@gmail.com>
2022-10-06 09:25:53 -0300

    teste

--- src/teste/teste.resolver.ts
+++ src/teste/teste.resolver.ts
@@ -1,6 +1,7 @@
 import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
 
 import { Teste } from './teste.model';
+const sleep = (ms: number) => new Promise((r) => setTimeout(r, ms));
 
 @Resolver(() => Teste)
 export class TesteResolver {
@@ -17,6 +18,8 @@ export class TesteResolver {
   @Mutation(() => Teste)
   async addTeste(@Args('counterInput') input: number) {
     this.testeCounter = input;
+
+    await sleep(1000);
     return {
       counter: this.testeCounter,
     };
